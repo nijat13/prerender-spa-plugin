6141c7fa73828005afc617043375443f571dd3ed
Nijat Ahmadov <nicataxmedov13@gmail.com>
2019-09-23 10:37:40 +0200

    Update index.js

--- es6/index.js
+++ es6/index.js
@@ -73,8 +73,8 @@ PrerenderSPAPlugin.prototype.apply = function (compiler) {
       })
       // Backwards-compatibility with v2 (postprocessHTML should be migrated to postProcess)
       .then((renderedRoutes) => {
-      console.log('renderedRoutes: ', renderedRoutes)
       console.log('this._options.postProcessHtml: ', this._options.postProcessHtml)
+      console.log('renderedRoutes: ', JSON.stringify(renderedRoutes))
       return this._options.postProcessHtml
         ? renderedRoutes.map(renderedRoute => {
           const processed = this._options.postProcessHtml(renderedRoute)
